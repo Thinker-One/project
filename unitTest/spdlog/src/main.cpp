eb3b31320a5aae9fb53ca50b7e0e7ed65e54f48f
root <root@debian.hnz>
2025-05-26 10:10:47 +0800

    移到test仓库

--- unitTest/spdlog/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <spdlog/spdlog.h>
-
-int main() {
-    spdlog::info("Welcome to spdlog!");  // 信息级别日志
-    spdlog::error("Something went wrong!");  // 错误级别日志
-    spdlog::warn("Warning: This is a warning.");  // 警告级别日志
-}
--- unitTest/thread/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "producerConsumer.hpp"
-
-int main() {
-    ProducerConsumer producer_consumer;
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    producer_consumer.stop_all();
-    return 0;
-}
